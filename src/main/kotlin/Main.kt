78af5db7047e184c8b9d38347173f5c428aae87f
Hugo <albrechthugo4@gmail.com>
2024-12-08 14:38:19 -0300

    collection practice 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
 package org.example
 
 fun main() {
-    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
-    val requested = "smtp"
-
-    val isSupported = requested.uppercase() in SUPPORTED
-
-    println("Support for $requested: $isSupported")
-
+    val number2word = mapOf(1 to "one", 2 to "two", 3 to "three")
+    val n = 2
+    println("$n is spelt as '${number2word[n]}'")
 }
