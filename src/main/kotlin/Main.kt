232f470b0fa7d6261a57f9049e9a2067cbf7df85
Hugo <albrechthugo4@gmail.com>
2024-12-08 20:49:48 -0300

    classes practice 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
-data class Employee(val name: String, var salary: Int)
+data class City(val name: String, val state: String)
+data class Address(val street: String, val city: City)
+data class Name(val firstName: String, val lastName: String)
+data class Person(val name: Name, val address: Address, val ownsAPet: Boolean = true)
 
 fun main() {
-    val emp = Employee("Mary", 20)
-    println(emp)
-    emp.salary += 10
-    println(emp)
+    val person = Person(
+        Name("John", "Smith"),
+        Address("123 Fake Street", City("Springfield", "US")),
+        ownsAPet = false
+    )
 }
