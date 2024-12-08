d41e924b88fab3871d4e9d0b8d3292839bc55078
Hugo <albrechthugo4@gmail.com>
2024-12-08 14:44:06 -0300

    control flow practice 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-package org.example
+import kotlin.random.Random
 
 fun main() {
-    val number2word = mapOf(1 to "one", 2 to "two", 3 to "three")
-    val n = 2
-    println("$n is spelt as '${number2word[n]}'")
+    val firstResult = Random.nextInt(6)
+    val secondResult = Random.nextInt(6)
+    val isDicesEqual = firstResult == secondResult
+
+    if (isDicesEqual) {
+        println("You win :)")
+    } else {
+        println("You lose :(")
+    }
 }
