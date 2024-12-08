f478e5f20951da187326c25d5efeee32c3fbb9ff
Hugo <albrechthugo4@gmail.com>
2024-12-08 15:23:17 -0300

    lambda practice 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
+fun repeatN(n: Int, action: () -> Unit) {
+    for (i in 1..n) {
+        action()
+    }
+}
+
 fun main() {
-    val actions = listOf("title", "year", "author")
-    val prefix = "https://example.com/book-info"
-    val id = 5
-
-    val urls = actions.map { action -> "${prefix}/${action}/$id" }
-
-    println(urls)
+    repeatN(5) {
+        println("Hello")
+    }
 }
