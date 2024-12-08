3976826149bbc424ddcd39ca959d4e03cfe49573
Hugo <albrechthugo4@gmail.com>
2024-12-08 14:57:44 -0300

    loops practice 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,22 @@
-fun main() {
-    val button = "A"
+// just while
+//fun main() {
+//    var pizzaSlices = 0
+//    while ( pizzaSlices < 7 ) {
+//        pizzaSlices++
+//        println("There's only $pizzaSlices slice/s of pizza :(")
+//    }
+//    pizzaSlices++
+//    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+//}
 
-    println(
-        when (button) {
-            "A" -> "Yes"
-            "B" -> "No"
-            "X" -> "Menu"
-            "Y" -> "Nothing"
-            else -> "There is no such button"
-        }
-    )
+// do while
+
+fun main() {
+    var pizzaSlices = 0
+    pizzaSlices++
+    do {
+        println("There's only $pizzaSlices slice/s of pizza :(")
+        pizzaSlices++
+    } while ( pizzaSlices < 8 )
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
 }
