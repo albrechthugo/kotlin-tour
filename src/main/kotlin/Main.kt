413f32a92eb63f9feadd6195b86f606e4a93cb8a
Hugo <albrechthugo4@gmail.com>
2024-12-08 15:19:38 -0300

    lambda practice 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-import kotlin.math.PI
-
-fun circleArea(radius: Int) = PI * radius * radius
-
 fun main() {
-    println(circleArea(2))
+    val actions = listOf("title", "year", "author")
+    val prefix = "https://example.com/book-info"
+    val id = 5
+
+    val urls = actions.map { action -> "${prefix}/${action}/$id" }
+
+    println(urls)
 }
