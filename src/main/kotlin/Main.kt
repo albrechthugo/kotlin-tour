ae671f5f92c161a77e9bdf30a0c2839cbaed8972
Hugo <albrechthugo4@gmail.com>
2024-12-08 21:05:54 -0300

    null safety practice 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,15 @@
-import kotlin.random.Random
+data class Employee (val name: String, var salary: Int)
 
-data class Employee(val name: String, var salary: Int)
-
-class RandomEmployeeGenerator(var minSalary: Int, var maxSalary: Int) {
-    private val availableEmployees = listOf("Max", "Oscar", "Lando")
-
-    fun generateEmployee(): Employee {
-        val salary = Random.nextInt(minSalary, maxSalary)
-        val name = availableEmployees.random()
-
-        return Employee(name, salary)
-    }
+fun employeeById(id: Int) = when(id) {
+    1 -> Employee("Mary", 20)
+    2 -> null
+    3 -> Employee("John", 21)
+    4 -> Employee("Ann", 23)
+    else -> null
 }
 
+fun salaryById(id: Int) = employeeById(id)?.salary ?: 0
 
 fun main() {
-    val empGen = RandomEmployeeGenerator(10, 30)
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    empGen.minSalary = 50
-    empGen.maxSalary = 100
-    println(empGen.generateEmployee())
+    println((1..5).sumOf { id -> salaryById(id) })
 }
